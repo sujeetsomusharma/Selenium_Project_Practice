04619e52f59e1bb293a41ccb41791f21877cd489
Sujeet Kumar Sharma <sujeetkumarsharma@Sujeets-MacBook-Air.local>
2024-04-13 18:55:49 +0530

    time.sleep value added

--- Envokebrowser.py
+++ Envokebrowser.py
@@ -62,7 +62,7 @@ time.sleep(5)
 
 print("Select the values form dropdown of news section")
 driver.find_element(By.PARTIAL_LINK_TEXT, "Spotlight").click()
-time.sleep(10)
 print("Spotlight is selected")
+time.sleep(5)
 
 print("---- END OF THE PAGE ----")
