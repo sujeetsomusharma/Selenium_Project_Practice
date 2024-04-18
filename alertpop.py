a19edf06e9975d586d484cff9e1571ff0a80aefd
Sujeet Kumar Sharma <sujeetkumarsharma@Sujeets-MacBook-Air.local>
2024-04-18 10:27:49 +0530

    alert popups

---
+++ alertpop.py
@@ -0,0 +1,34 @@
+import time
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import ui
+from selenium.webdriver.support.select import Select
+
+print("Selenium Version you are using = ", selenium.__version__)
+
+# driver = webdriver.Edge()
+driver = webdriver.Chrome()
+# driver = webdriver.Firefox()
+'''This is for when we have have offline chrome browser and chrome driver if vpn resrticted the driver access 
+other wise we can user direct by using driver = webdriver.Chrome() driver.get("https://www.cricbuzz.com/")
+'''
+# s = Service("C:\Users\sujeet.sharma\Desktop\chromedriver-win64\chromedriver-win64\chromedriver.exe")
+# driver = webdriver.Chrome(service=s)
+
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+print("---Browser is open---")
+time.sleep(2)
+
+driver.maximize_window()
+print("---Window maximize---")
+time.sleep(2)
+
+print("---The Title of the page is---")
+print("The tile is = ", driver.title)
+time.sleep(2)
+
+print("---The url of the page is--- ")
+print("The url of the page is = ", driver.current_url)
+time.sleep(2)
