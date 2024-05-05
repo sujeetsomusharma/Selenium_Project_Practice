c9657fd249f6568ce0efe927892883edaef1c252
Sujeet Kumar Sharma <sujeetkumarsharma@Sujeets-MacBook-Air.local>
2024-05-05 18:20:06 +0530

    pytest learning about the decorator in pytest

---
+++ pytestdemo/test_credit.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def test_creditNumber1():
+    a = 4
+    b = 6
+    assert a + b == 10, "Credit card number is correct"
+
+
+@pytest.mark.skip
+def test_creditNumber2():
+    a = 4
+    b = 6
+    assert a + b == 12, "Credit card number is not correct"
+
+
+@pytest.mark.xfail(reason="This is a failed case which is not expected as per requirement")
+def test_fail_case():
+    print("Failed Case")
--- pytestdemo/test_demo1.py
+++ pytestdemo/test_demo1.py
@@ -1,11 +1,38 @@
 # Any pytest file start with "test_"
 # pytest method start with test only
-# any coe should be wrapped in method only
+# any code should be wrapped in method only
+# In pytest we can't have multiple test method name with the same and
+# if it exists that means it will override the previous case.
+# to run the scripts there are various method just like pytest.py -s -v -k
+# -s is for logs in output, -k is to run specific methods using rgex, -v is for logs with more info with meta data,
+# also we can run specifc file with file name like pytest.py "file name" -v -s -k.
+# we can also run the specific method that we need to run smoke test with "@pytest.mark.smoke" with the flag name -m
+# "@pytest.mark.skip" is to skip any method in pytest
+# if any bug in any method but still want to run but with no output then we can use "@pytest.mark.xfail"
+
+# In pytest, @pytest.mark is a decorator used for applying markers to tests and test functions. Markers are a way to
+# add metadata or attributes to tests, allowing you to control how they are run or to categorize them for various
+# purposes.
+
+# You can use @pytest.mark to apply built-in markers provided by pytest or create your own custom markers. Here are
+# some common uses:
+
+# Built-in Markers:
+# @pytest.mark.skip: Skips a test without running it.
+# @pytest.mark.xfail: Marks a test as expected to fail.
+# @pytest.mark.parametrize: Provides multiple sets of arguments to a test function.
+
+import pytest
+
+import pytestdemo
+
 
 def test_firstDemo():
     print("Hello")
 
 
+@pytest.mark.smoke
 def test_secondDemo():
-    msg = "HI Good Morning"
-    assert msg == "hi", "This is not the correct message"
+    msg = "Hi Good Morning"
+    assert msg == "Hi Good Morning"
+    # assert msg == "hi", "This is not the correct message"
